728f94b61f7fd0d6429665b2932e8a4794f78e43
Ayaz Mehmood <ayaz.ebridge@gmail.com>
2026-04-21 20:47:37 +0500

    Revert "feat: add user schema to sanity"

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,8 +4,7 @@ import { categoryType } from "./categoryType";
 import { contentSectionType } from "./contentSectionType";
 import { postType } from "./postType";
 import { siteSettings } from "./siteSettings";
-import { userType } from "./userType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType, contentSectionType, postType, siteSettings, userType],
+  types: [categoryType, contentSectionType, postType, siteSettings],
 };
